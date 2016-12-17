7b94b77ba19e05b468ecccd18e802b7399dfea2f
Michael Prather <mprather1@gmail.com>
2016-12-17 07:50:42 +0000

    redis sessions

--- _config.js
+++ _config.js
@@ -3,7 +3,12 @@ var config = {};
 config.postgresURI = {
   development: "postgres://postgres:postgres@localhost:5432/api_development",
   test: "postgres://postgres:postgres@localhost:5432/api_test",
-  production: "postgres://postgres:postgres@localhost:5432/api_production"
+  production: process.env.POSTGRES_URI,
+};
+
+config.redisStore = {
+  url: process.env.REDIS_STORE_URI,
+  secret: process.env.REDIS_STORE_SECRET
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -2,11 +2,24 @@ var express = require("express");
 var app = express();
 var path = require("path")
 var bodyParser = require("body-parser");
+var config = require("./_config");
+var session = require("express-session");
+var RedisStore = require("connect-redis")(session);
 var routes = require("./routes");
 var port = process.env.PORT || '8000';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(session({
+  store: new RedisStore({
+    url: config.redisStore.url
+  }),
+  secret: config.redisStore.secret,
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(express.static(path.join(__dirname, 'app/static')));
 
 app.use('/api', routes);
