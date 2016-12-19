48fb6092ac4026f59ad7ef7d4158e163b4212edd
Michael Prather <mprather1@gmail.com>
2016-12-19 01:56:46 +0000

    morgan

--- routes.js
+++ routes.js
@@ -1,17 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var time = require("./helpers").time;
 var db = require("./queries");
-
-if (process.env.NODE_ENV === 'development'){
-  router.use(function(req, res, next){
-    var d = new Date();
-    var connection = req.connection.remoteAddress.split(":");
-    var connectionString = connection[connection.length - 1];
-    console.log("%s - %s - %s => %s", connectionString, time.currentTime(d), req.url, req.method);
-    next();
-  });
-}
 
 router.route("/models")
   .get(db.getAllModels)
--- server.js
+++ server.js
@@ -3,22 +3,16 @@ var app = express();
 var path = require("path")
 var bodyParser = require("body-parser");
 var config = require("./_config");
-// var session = require("express-session");
-// var RedisStore = require("connect-redis")(session);
+var morgan = require("morgan")
 var routes = require("./routes");
 var port = process.env.PORT || '8000';
 
+if (process.env.NODE_ENV === "development"){
+  app.use(morgan('dev'));
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// app.use(session({
-//   store: new RedisStore({
-//     url: config.redisStore.url
-//   }),
-//   secret: config.redisStore.secret,
-//   resave: false,
-//   saveUninitialized: false
-// }));
 
 app.use(express.static(path.join(__dirname, 'app/static')));
 
